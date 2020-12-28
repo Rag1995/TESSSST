89654524a3406243b811c8bc5484dfb239bfbfa3
Taylor Kuo <taipeiyyy@gmail.com>
2020-12-28 16:48:34 +0800

    deploy

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <!-- Load Facebook SDK for JavaScript -->
-    <div id="fb-root"></div>
+    <!-- <div id="fb-root"></div> -->
     <!-- Your Chat Plugin code -->
-    <div class="fb-customerchat"
+    <!-- <div class="fb-customerchat"
       attribution=setup_tool
       page_id="179163795468160">
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -20,28 +20,28 @@ export default {
   components: {
     HelloWorld
   },
-  created() {
-    // 防止重複載入
-    if (!window.FB) {
-      window.fbAsyncInit = () => {
-        window.FB.init({
-          // appId: '{your-app-id}', // 填入自己 app 的 id
-          // cookie: true,
-          xfbml: true,
-          version: 'v9.0' // 目前版本
-        });
-      };
+  // created() {
+  //   // 防止重複載入
+  //   if (!window.FB) {
+  //     window.fbAsyncInit = () => {
+  //       window.FB.init({
+  //         // appId: '{your-app-id}', // 填入自己 app 的 id
+  //         // cookie: true,
+  //         xfbml: true,
+  //         version: 'v9.0' // 目前版本
+  //       });
+  //     };
 
-      ((d, s, id) => {
-        const fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        const js = d.createElement(s);
-        js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      })(document, 'script', 'facebook-jssdk');
-    }
-  }
+  //     ((d, s, id) => {
+  //       const fjs = d.getElementsByTagName(s)[0];
+  //       if (d.getElementById(id)) return;
+  //       const js = d.createElement(s);
+  //       js.id = id;
+  //       js.src = 'https://connect.facebook.net/en_US/sdk.js';
+  //       fjs.parentNode.insertBefore(js, fjs);
+  //     })(document, 'script', 'facebook-jssdk');
+  //   }
+  // }
 }
 </script>
 
